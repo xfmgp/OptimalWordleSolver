a9faf58c9b9dfc4e9c0c7eea29571d0ffd1ec9b6
xfmgp <ycarp@pm.me>
2026-09-12 16:09:50 +0800

    Update words.rs

--- src/words.rs
+++ src/words.rs
@@ -92,13 +92,14 @@ pub fn hint_to_str(mut hint: u8) -> String {
 }
 
 lazy_static! {
-    pub static ref GUESS_WORDS   : Vec<String> = read_words("words/guess_words.txt"   );
+    
     // pub static ref GUESS_WORDS   : Vec<String> = read_words("words/solution_words.txt");
     // pub static ref SOLUTION_WORDS: Vec<String> = read_words("words/solution_words.txt");
 
     // pub static ref GUESS_WORDS   : Vec<String> = read_words("words/guess_words_2.txt"   );
     // pub static ref SOLUTION_WORDS: Vec<String> = read_words("words/solution_words_2.txt");
 
+    pub static ref GUESS_WORDS   : Vec<String> = read_words("words/guess_words.txt");
     pub static ref SOLUTION_WORDS: Vec<String> = read_words("words/solution_words_wordlecup.txt");
     
 
