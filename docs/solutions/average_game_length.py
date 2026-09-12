c6a1e4d06a234c10ec4c1d687aa5ab95328a4c9f
xfmgp <ycarp@pm.me>
2026-09-12 14:17:48 +0800

    Delete docs directory

--- docs/solutions/average_game_length.py
+++
@@ -1,9 +0,0 @@
-with open("2309_solution.txt") as file:
-    average = 0
-    denom = 0
-
-    for line in file:
-        average += len(line.split()) // 2
-        denom += 1
-
-    print(average / denom)
